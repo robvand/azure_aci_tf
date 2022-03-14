3e2cdd93e1adf7d327b619e4a80a33ee79a84c96
Rob van der Kind <r.vdkind@hotmail.com>
2022-03-14 21:37:49 +0100

    changing azurerm version

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.44.0"
+      version = "2.99.0"
     }
     mso = {
       source  = "CiscoDevNet/mso"
@@ -10,7 +10,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.0.1"
+      version = "2.8.0"
     }
   }
 }
